cd33c0c3cfb71cc2652b0d5da27c6c1df4ee24ca
Silje32 <msh070425@gmail.com>
2025-04-23 12:31:19 +0200

    Added the ThemeSwitcher function

--- themeToggle.js
+++ themeToggle.js
@@ -1,1 +1,12 @@
 const toggleButton = document.getElementById("toggle-btn");
+
+/*
+ * Switch the background of our website
+ */
+
+function themeSwitcher() {
+  document.body.classList.toggle("dark-mode");
+  console.log("The theme switch button was clicked");
+}
+
+toggleButton.addEventListener("click", themeSwitcher);
